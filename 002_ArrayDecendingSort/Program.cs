44f073f38ca773706a4b52aefdbf2c9e443f822f
Sagi Nedunkanal <sagi.nedunkanal@gmail.com>
2013-09-29 15:18:46 +0200

    Projekt 002_ArrayDecendingSort hinzugefügt

---
+++ 002_ArrayDecendingSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _002_ArrayDecendingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 6, 2, 5 };
+
+            Console.WriteLine("Array:          " + String.Join(", ", array));
+
+            Array.Reverse(array);
+            Console.WriteLine("Reverse:        " + String.Join(", ", array));
+
+            Array.Sort(array);
+            Array.Reverse(array);
+            Console.WriteLine("Decending sort: " + String.Join(", ", array));
+        }
+    }
+}
