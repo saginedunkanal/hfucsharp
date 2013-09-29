09489d4f47e98c0bfe1f9f3cd02c9c8a884270ba
Sagi Nedunkanal <sagi.nedunkanal@gmail.com>
2013-09-29 15:16:49 +0200

    Projekt 001_MakeFormatStringVariable hinzugefügt

---
+++ 001_MakeFormatStringVariable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _001_MakeFormatStringVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello {0:F5}", 0);
+
+            // Variante 1
+            string output1 = "Hello {0:F5}";
+            output1 = output1.Replace("{0:F5}", "{0:F10}");
+            Console.WriteLine(output1, 1);
+
+            // Variante 2
+            int numberOfDigits = 20;
+            string output2 = "Hello {0:F" + numberOfDigits + "}";
+            Console.WriteLine(output2, 2);
+        }
+    }
+}
